19413bbdc3791d43a680d53018b9ec095f9761af
kzlee <kzlee1612@gmail.com>
2026-01-29 22:16:49 +0800

    Update MenuID to MenuId

--- PRG2_assignment/Menu.cs
+++ PRG2_assignment/Menu.cs
@@ -6,21 +6,21 @@ namespace PRG2_assignment
 {
     class Menu
     {
-        public string MenuID { get; set; }
+        public string MenuId { get; set; }
         public string MenuName { get; set; }
 
         public List<FoodItem> FoodItemList { get; set; }
 
         public Menu()
         {
-            MenuID = "";
+            MenuId = "";
             MenuName = "";
             FoodItemList = new List<FoodItem>();
         }
 
         public Menu(string menuID, string menuName, List<FoodItem> initialFoods)
         {
-            MenuID = menuID;
+            MenuId = menuID;
             MenuName = menuName;
             
             FoodItemList = initialFoods ;
@@ -52,7 +52,7 @@ namespace PRG2_assignment
 
         public override string ToString()
         {
-            return $"Menu ID is: {MenuID}, Menu name is: {MenuName}";
+            return $"Menu ID is: {MenuId}, Menu name is: {MenuName}";
         }
     }
 }
