622a1c63e38d658bc2803dc9dba8c0dfdd060381
kzlee <kzlee1612@gmail.com>
2026-01-29 20:30:32 +0800

    Implemented SpecialOffer class according to diagram

---
+++ PRG2_assignment/SpecialOffer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRG2_assignment
+{
+    class SpecialOffer
+    {
+        public string OfferCode { get; set; }
+        public string OfferDesc { get; set; }
+        public double Discount { get; set; }
+
+        public SpecialOffer() { }
+
+        public SpecialOffer(string offerCode, string offerDesc, double discount)
+        {
+            OfferCode = offerCode;
+            OfferDesc = offerDesc;
+            Discount = discount;
+        }
+
+        public override string ToString()
+        {
+            return $"Offer code: {OfferCode} Offer description: {OfferDesc} Discount: {Discount}%";
+        }
+    }
+}
